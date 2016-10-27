4c2f64f12a2893027643ededd2d0b37e9023a792
Adi Sanduri <S8011477@RS.HT>
2016-10-27 10:44:09 -0700

    add crud

--- routes/crud.js
+++ routes/crud.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 var Unit = require('../models/unitSchema');
 var Patient = require('../models/patientSchema');
 
+function sortArrayByLastTimestamp(arrayToSort) {
+
+    arrayToSort.sort(function(a,b) {
+        return parseFloat(b.timestamp) - parseFloat(a.timestamp); 
+    });
+}
+
 crudRouter.get('/units', function (req, res, next) {
     Unit.find(function (err, units) {
         if (err) {
@@ -44,6 +51,7 @@ crudRouter.get('/patients', function(req, res, next) {
     });
 });
 
+// Get patient by id
 crudRouter.get('/patients/:id', function(req, res,next) {
     Patient.findOne({"id": req.params.id, function(err, patient) {
         if (err) {
@@ -54,31 +62,35 @@ crudRouter.get('/patients/:id', function(req, res,next) {
     }});
 });
 
+// Get patients by unit id
 crudRouter.get('/patients/units/:unitId', function(req, res,next) {
     Patient.find({"CurrentStation" : req.params.id}, "braceletId measurements",  function(err, patients) {
         if (err) {
             res.send(err);
         } else {
             var result = []; 
+            
+            // Run all patients and save specific fields
             patients.forEach(function(patient){
+
+                // Create new json which inculdes the following fields:
+                // braceletId, temperature, storation, bloodPressure, heartbeat
+                // Sort each array in measurements according to last timestamps and set the last into json
                 var newPatient = {
                                     "braceletId" : patient.braceletId,
-                                    "temperatures" : undefined,
-                                    "storations" : undefined,
-                                    "bloodPressures" : undefined,
-                                    "heartbeat" : undefined };
-                
-                // sort each array
-                arrayToSort.sort(function(a,b) {
-                    //return parseFloat() - parseFloat
-                })
+                                    "temperature" :  sortArrayByLastTimestamp(patient.measurements.temperatures)[0].tempreature,
+                                    "storation" : sortArrayByLastTimestamp(patient.measurements.storations)[0].storation,
+                                    "bloodPressure" : sortArrayByLastTimestamp(patient.measurements.bloodPressures)[0].bloodPressure,
+                                    "heartbeat" : sortArrayByLastTimestamp(patient.measurements.heartbeat)[0].heartbeat };
+                result.push(newPatient);                
+            });
 
-            })
-            res.send();
+            res.send(result);
         }    
     })
 });
 
+// Get the name of unit by id
 crudRouter.get('/units/:id', function(req, res,next) {
     Patient.findOne({"id": req.params.id} , "name", function(err, unit) {
         if (err) {
@@ -88,6 +100,37 @@ crudRouter.get('/units/:id', function(req, res,next) {
         }
     });
 });
+
+// Get all units under specific unit
+crudRouter.get('/units/:unitId/units', function(req, res, next) {
+    var list = [];
+    var units = Unit.find();
+    var pattern = "^" + req.params.unitId + "[_\d]{1}[0-9]+$";
+    var regex = new RegExp(pattern);
+    var bIsIdExist = false;
+
+    // Find if the unit id is existed
+    for (var i=0; i<units.length; i++) {
+        if(units[i].id === req.params.unitId)
+        bIsIdExist = true;
+    }
+    
+    // If the id exist
+    if (bIsIdExist) {
+        
+        // Run all units and find if the are units which match the regex
+        for (var i=0; i<units.length; i++) {
+            if (regex.test(units[i].id))
+                list.push(units[i]);
+        }
+    }
+    res.json(list);
+});
+
+// Update patient details
+crudRouter.update('/patients/:object', function (req, res, next) {
+    //Patient.find
+})
 
 crudRouter.delete('/patients/:id', function (req, res, next) {
     Patient.findOneAndRemove({"id": req.params.id}, function(err, unit) {
