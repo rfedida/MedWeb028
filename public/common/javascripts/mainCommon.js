7a7896751c61e93f2f27300e829583c606206eca
Lior Hirsh <s8096907@RS.HT>
2016-10-26 23:55:23 -0700

    hello

--- public/common/javascripts/mainCommon.js
+++ public/common/javascripts/mainCommon.js
@@ -1,9 +1,28 @@
-var medApp = angular.module('medApp', []);
+var medApp = angular.module('medApp', ["ngMaterial"]);
 medApp.controller('medController',['$scope', '$http', function ($scope, $http){
 
    
     $scope.data = "try";
 }]);
+
+angular.module('medApp').config(function($provide, $httpProvider){
+    $provide.factory("ErrorInterceptor", function($q) {
+        return {
+            responseError : function(rejection)
+            {
+                console.log(rejection);
+                return $q.reject(rejection);
+            }
+        };
+    })
+
+    $httpProvider.interceptors.push("ErrorInterceptor");
+});
+
+angular.module("medApp").controller("mainController", function($http, $scope, loginCardService)
+{
+    loginCardService.showLoginCard();
+})
 
 
 
