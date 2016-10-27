8a9d7b230d46ce9a339053598a566ccec6f13bfd
unknown <Hashlama@RS-APPA3.RS.HT>
2016-10-27 15:16:43 -0700

    add new ng-view files

---
+++ public/med/javascripts/main.js
@@ -0,0 +1,27 @@
+var app = angular.module("medApp", ["ngRoute"]);
+
+app.config(['$routeProvider', 
+function($routeProvider){
+	$routeProvider
+		.when('/', {
+		templateUrl: "/med/views/view0.html"
+		})
+		.when('/view1', {
+		templateUrl: "/med/views/view1.html"
+		})
+		.when('/view2', {
+		templateUrl: "/med/views/view2.html"
+		})
+		.when('/view3', {
+		templateUrl: "/med/views/view3.html"
+		});
+}]);
+
+app.controller('GIL', function($scope, $location){
+	$scope.message = 'AAA';
+	$scope.path = "start";
+	$scope.go = function(path){
+		$scope.path=path;
+		$location.path = path;
+	}
+});
