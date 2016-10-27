c6269b89ee8f6152e6f4234182c97cb6ff1f440a
Tali Cohen <s8065506@RS.HT>
2016-10-27 17:32:21 +0300

    hi

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ var agamRoutes = require('./routes/agam');
 var medRoutes = require('./routes/med');
 var infrastructureRoutes = require('./routes/infrastructure');
 var mongoose = require('mongoose');
-
-
+var crud = require('./routes/crud');
 var app = express();
-
 app.use(function(req, res, next)
 {
     var loginDetails = {};
     var isLoggedOn = true;
     console.log("req address : " + req.originalUrl);
     console.log("check if user logged on");
-
     if (isLoggedOn)
     {
         next();
@@ -25,30 +22,23 @@ app.use(function(req, res, next)
         res.status(401).send();
     }
 });
-
 app.use(express.static(__dirname + '\\public'));
-
 app.use('/', routes);
 app.use('/api/agam', agamRoutes);
 app.use('/api/med', medRoutes);
 app.use('/api/infrastructure', infrastructureRoutes);
-
-
+app.use('/crud', crud);
 // Listening to port 9000
 var port = process.env.PORT || 9000;
 app.listen(port, function() {
     console.log('Listening on ' + port);
 });
-
 // Connect to mongoDB
-mongoose.connect('mongodb://150.0.0.228:27017/medicineDB');
-
+mongoose.connect('mongodb://150.0.0.56:27017/DB');
 // Getting the data from the db
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
     console.log("connect to mongo");
 });
-
 module.exports = app;
-
--- server/agam/OccupationControl.js
+++ server/agam/OccupationControl.js
@@ -1,10 +1,14 @@
-var Patient = require('../common/nothing.js');
+var Patient = require('../../models/patientSchema.js');
 
-function getOcupationAmoutGraph(hirarchCode){
+ var  getOcupationAmoutGraph = function(hirarchCode){
 
+     Patient.find(function(err, res){
+         console.log(res);
+     });
     // checking the location in the hirarchy
-    if (hirarchCode.length == 1) // pikud
-    {
+    switch (hirarchCode.length) {
+        case 1: // pikud
+        {
         Patient.aggregate([
         {"$match": {"CurrentStation" : {"$regex": "/^" + hirarchCode + "/"}}},
         {"$project": { "title": 2, "CurrentStation": {"$substr": ["$CurrentStation", 0, 3]}, "Emergency": "$General_Data.Emergency"}},
@@ -21,5 +25,105 @@ function getOcupationAmoutGraph(hirarchCode){
             function(res, err){
 
             });
+            break;
+        }
+        case 3: // ugda
+        {
+            // Get all the taagads in the hirarchy
+            Patient.aggregate(
+                [{"$match": {"CurrentStation" : {"$regex": "/^" + hirarchCode + ".{4}$/"}}},
+                {"$group" : {
+                    "_id" : {"Station": "$CurrentStation",
+                        "Emergency": "$General_Data.Emergency"},
+                    "count": {"$sum": 1}
+                    }},
+                {"$group":{
+                    "_id" : "$_id.Emergency",
+                    "values": { "$push" : {"x": "$_id.Station", "y": "$count"}}}    
+                }
+                ], function(res, err){
+                    console.log(res);
+                }
+            );
+
+            // Get all the palhaks in the hirarchy
+            Patient.aggregate(
+                [{"$match": {"CurrentStation" : {"$regex": "/^" + hirarchCode + ".{2}$/"}}},
+                {"$group" : {
+                    "_id" : {"Station": "$CurrentStation",
+                        "Emergency": "$General_Data.Emergency"},
+                    "count": {"$sum": 1}
+                    }},
+                {"$group":{
+                    "_id" : "$_id.Emergency",
+                    "values": { "$push" : {"x": "$_id.Station", "y": "$count"}}}    
+                }
+                ]
+            ).exec(function (err, res)
+            { 
+                console.log(res)
+            });         
+        }
+        case 5: // palhak
+        {
+            // Get all the taagads in the hirarchy
+            Patient.aggregate(
+                [{"$match": {"CurrentStation" : {"$regex": "/^" + hirarchCode + ".{4}$/"}}},
+                {"$group" : {
+                    "_id" : {"Station": "$CurrentStation",
+                        "Emergency": "$General_Data.Emergency"},
+                    "count": {"$sum": 1}
+                    }},
+                {"$group":{
+                    "_id" : "$_id.Emergency",
+                    "values": { "$push" : {"x": "$_id.Station", "y": "$count"}}}    
+                }
+                ], function(res, err){
+                    
+                }
+            );
+
+            // Get the current palhak 
+            Patient.aggregate([
+            {"$match": {"CurrentStation" :hirarchCode}},
+            {"$group" : {
+                  "_id" : {"Station": "$CurrentStation",
+                         "Emergency": "$General_Data.Emergency"},
+                  "count": {"$sum": 1}
+             }},
+            {"$group":{
+                   "_id" : "$_id.Emergency",
+                   "values": { "$push" : {"x": "$_id.Station", "y": "$count"}}}    
+            }], function(res, err) {
+
+            });
+
+            break;
+        }
+        case 7: // taagad
+        {
+            Patient.aggregate([
+            {"$match": {"CurrentStation" :hirarchCode}},
+            {"$group" : {
+                  "_id" : {"Station": "$CurrentStation",
+                         "Emergency": "$General_Data.Emergency"},
+                  "count": {"$sum": 1}
+             }},
+            {"$group":{
+                   "_id" : "$_id.Emergency",
+                   "values": { "$push" : {"x": "$_id.Station", "y": "$count"}}}    
+            }], function(res, err) {
+
+            });
+
+            break;
+        }
+        default:
+            break;
     }
 }
+
+module.exports = 
+{
+    getOcupationAmoutGraph : getOcupationAmoutGraph
+};
