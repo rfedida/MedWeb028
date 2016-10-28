37c24a1ec9d12d01ef8540c724797c9fa1c8e1da
Noa Vaknin <s8089381@RS.HT>
2016-10-28 17:34:02 +0300

    styled pie charts

--- public/agam/javascripts/typeOfInjuryCtrl.js
+++ public/agam/javascripts/typeOfInjuryCtrl.js
@@ -29,42 +29,73 @@ myApp.controller('statisticController', function($scope, $http) {
                 d3.selectAll('.nv-pieLabels text').style('fill', 'black');                
             }
         }        
-    };
-    //trying
-    $scope.injury = [];
+    };  
 
-<<<<<<< HEAD
-    $scope.lineChartOptions = {
-        chart:
-        {
-            type: 'lineChart',
-            height: 350,
-            width: 350,
-            x: function(d){return d.key},
-            y: function(d){return d.y},
-            color: function(d, i) {
-                var colorArray = ['#000000', '#660000', '#CC0000', '#FF6666', '#FF3333', '#FFE6E6'];                 
-                return colorArray[i];        
-            },
-            duration: 500,
-            xAxis:
-            {
-                axisLable: 'שעה'
-            },
-            yAxis:
-            { 
-                axisLable: 'מספר נפגעים',
-                axisLableDistance: 0
-            }
-        }        
-    };
-
+    // $scope.lineChartOptions = {
+    //     chart:
+    //     {
+    //         type: 'lineChart',
+    //         height: 350,
+    //         width: 350,
+    //         x: function(d){return d.key},
+    //         y: function(d){return d.y},
+    //         color: function(d, i) {
+    //             var colorArray = ['#000000', '#660000', '#CC0000', '#FF6666', '#FF3333', '#FFE6E6'];                 
+    //             return colorArray[i];        
+    //         },
+    //         duration: 500,
+    //         xAxis:
+    //         {
+    //             axisLable: 'שעה'
+    //         },
+    //         yAxis:
+    //         { 
+    //             axisLable: 'מספר נפגעים',
+    //             axisLableDistance: 0
+    //         }
+    //     }        
+    // };
     //$http.get('./crud/patients').success(function(response) {
     //    $scope.injuryLocationData = response.data;
     //}).error(function(err){
     //    throw err;
     //});
 
+    $scope.injury = [];
+    $http.get("/crud/injuryMechanism").success(function(data){
+        $scope.injury = data;
+      
+        $scope.injuryMechanismData = [
+            {
+                key: 'תלול מסלול',
+                y: $scope.injury[0].generalData.emergency
+            },
+            {
+                key: 'ירי',
+                y: 36
+            },
+            {
+                key: 'אב"כ',
+                y: 8
+            },
+            {
+                key: 'כוויה',
+                y: 49
+            },
+            {
+                key: 'שאיפה',
+                y: 49
+            },
+            {
+                key: 'תאונת דרכים',
+                y: 49
+            }
+       ];
+    }).error(function(data){
+        console.log(data);
+    });
+    
+    
     $scope.injuryLocationData = [
         {
             key: 'פגיעות מפשעה',
@@ -83,84 +114,5 @@ myApp.controller('statisticController', function($scope, $http) {
             y: 49
         }
     ];
-
-    $scope.data = [
-        {
-            x: 1,
-            y: 7
-        },
-        {
-            x: 2,
-            y: 36
-        },
-        {
-            x: 3,
-            y: 8
-        },
-        {
-            x: 4,
-            y: 49
-        }
-    ];
-
-    $scope.injuryTypeData = [
-=======
-    $http.get("/crud/injuryMechanism").success(function(data){
-     $scope.injury = data;  
-      
-
-      $scope.data2 = [
->>>>>>> 566f11171063f19e0add15d4be787e38ed42c1e2
-        {
-            key: 'תלול מסלול',
-            y: $scope.injury[0].generalData.emergency
-        },
-        {
-            key: 'ירי',
-            y: 36
-        },
-        {
-            key: 'אב"כ',
-            y: 8
-        },
-        {
-            key: 'כוויה',
-            y: 49
-        },
-        {
-            key: 'שאיפה',
-            y: 49
-        },
-        {
-            key: 'תאונת דרכים',
-            y: 49
-        }
-
-    ];
-
-        }).error(function(data){
-        console.log(data);
-    });
-    
-    
-    $scope.data1 = [
-        {
-            key: 'פגיעות מפשעה',
-            y: 7
-        },
-        {
-            key: 'פגיעות חזה',
-            y: 36
-        },
-        {
-            key: 'פגיעות גפיים',
-            y: 8
-        },
-        {
-            key: 'פגיעות ראש',
-            y: 49
-        }
-    ];
-
    
 });
