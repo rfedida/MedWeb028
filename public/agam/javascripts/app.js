f12355194175da20bbac86a0eefde6b14969911c
Tomer Belkin <s7857357@RS.HT>
2016-10-27 23:46:44 -0700

    add tree without data

--- public/agam/javascripts/app.js
+++ public/agam/javascripts/app.js
@@ -1,5 +1,7 @@
-var myApp = angular.module('myApp', ['nvd3', 'leaflet-directive']);
-myApp.controller('statisticController', function musicController($scope, $http) {
+var myApp = angular.module('myApp', ['nvd3', 'treeControl', 'leaflet-directive']);
+myApp.controller('statisticController', function ($scope, $http) {
+    $scope.treeInd = true;
+$scope.window = "col-md-6";
     $scope.options = {
         chart:
         {
@@ -41,4 +43,45 @@ myApp.controller('statisticController', function musicController($scope, $http) 
             y: 49
         }
     ];
+
+    
+    $scope.treeEvent = function(){
+        $scope.treeInd = !$scope.treeInd;
+       
+        if ( $scope.window == "col-md-6"){
+             $scope.window = "col-md-8";
+        }else{
+            $scope.window = "col-md-6"
+        }
+    }
+});
+myApp.controller('unitsTreeController', function ($scope, $http) {
+      $scope.treeOptions = {
+          nodeChildren: "children",
+          dirSelectable: true,
+          injectClasses: {
+              ul: "a1",
+              li: "a2",
+              liSelected: "a7",
+              iExpanded: "a3",
+              iCollapsed: "a4",
+              iLeaf: "a5",
+              label: "a6",
+              labelSelected: "a8"
+          }
+      }
+      $scope.dataForTheTree = 
+      [
+          {"name" : "Joe", "age" : "21", "children" : [
+              {"name" : "Smith", "age" : "42", "children" : []},
+              {"name" : "Gary", "age" : "21", "children" : [
+                  {"name" : "Jenifer", "age" : "23", "children" : [
+                      {"name" : "Dani", "age" : "32", "children" : []},
+                      {"name" : "Max", "age" : "34", "children" : []}
+                  ]}
+              ]}
+          ]},
+          {"name" : "Albert", "age" : "33", "children" : []},
+          {"name" : "Ron", "age" : "29", "children" : []}
+      ];
 });
