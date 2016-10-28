e02afe45a742b8064dc6585c3eaffb74835451ea
Tomer Belkin <s7857357@RS.HT>
2016-10-28 06:19:17 -0700

    add find unis according the user id

--- routes/agam.js
+++ routes/agam.js
@@ -2,19 +2,32 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var graphOccupation = require('../server/agam/OccupationControl.js');
+var Units = require('../../models/unitSchema');
 
 /* GET home page of agam. */
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname,'../public/agam/views/agamView.html'));
 });
 
-   router.get('/Occupation/:userHirarchy', function(req, res, next){
+router.get('/Occupation/:userHirarchy', function(req, res, next){
   var x= res.req.params;
   var userHirarchy = res.req.params.userHirarchy;
   graphOccupation.getOcupationAmoutGraph(userHirarchy, function(fullJson) {
       res.send(fullJson);
   });
   
+});
+
+router.get('/units/:userHirarchy', function(req, res, next){
+  var x= res.req.params;
+  var userHirarchy = res.req.params.userHirarchy;
+  Units.find({'id': new RegExp('^'+hirarchCode)}, function(err, units) {
+            if (err) { 
+                res.send(err);
+            } else {
+                res.send(units);
+            }
+    });
 });
 
 module.exports = router;
