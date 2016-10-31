496a30d90737fc900a19642a716d2cd5621e7bf0
unknown <Hashlama@RS-APPA3.RS.HT>
2016-10-31 11:50:00 -0700

    Some changes

--- public/med/javascripts/injInfo.js
+++ public/med/javascripts/injInfo.js
@@ -60,13 +60,18 @@ angular.module("medApp").controller('InjuredController', ['$scope', 'medAppFacto
                 modal.element.modal();
                 modal.close.then(function (result) {
                     var newTreat = angular.copy(medAppFactory.newTreatment);
-                   newTreat.treatmentType = $scope.selectedTreatMed;
-                    newTreat.location = $scope.treatLocation;
-                    newTreat.bloodPressure = $scope.highBloodPressure + "/" + $scope.lowBloodPressure;
-                    newTreat.heartbeat = $scope.heartbeat;
-                    newTreat.temperature = $scope.temperature;
-                    newTreat.storation = $scope.storation;
-                });
+                   newTreat.treatmentType = result.selectedTreatMed;
+                    newTreat.location = result.treatLocation;
+                    newTreat.bloodPressure =result.highBloodPressure + "/" + result.lowBloodPressure;
+                    newTreat.heartbeat = result.heartbeat;
+                    newTreat.temperature = result.temperature;
+                    newTreat.storation = result.storation;
+                    $scope.treatments.push(newTreat);
+
+                     $http.put('/crud/patients', { "patient": $scope.injured }).then(function (response) {
+
+                    });   
+             });
                 debugger;
             });
         };
@@ -86,11 +91,13 @@ angular.module("medApp").controller('InjuredController', ['$scope', 'medAppFacto
 
 */
 
-        /******************  SHIR *****************************************************************************************/
+/******************  SHIR *****************************************************************************************/
         $scope.medicationId = "medicationId";
         $scope.liquidId = "medicationId";
-    }]);
+/******************  SHIR *****************************************************************************************/    
+}]);
 
+/******************  Gil *****************************************************************************************/
 angular.module("medApp").controller('ComplexControllerOperation', [
     '$scope', '$element', '$filter', 'title', 'close',
     function ($scope, $element, $filter, title, close) {
@@ -105,9 +112,12 @@ angular.module("medApp").controller('ComplexControllerOperation', [
         //  the button has the 'data-dismiss' attribute.
         $scope.close = function () {
             close({
-                braceId: $scope.braceId,
-                date: $scope.date,
-                time: $scope.time
+                selectedTreatMed: selectedTreatMed.selectedIndex,
+                treatLocation: treatLocation.value,
+                bloodPressure: highBloodPressure.value + "/" + lowBloodPressure.value,
+                heartbeat: heartbeat.value,
+                temperature: temperature.value,
+                storation: storation.value
             }, 500); // close, but give 500ms for bootstrap to animate
         };
 
@@ -120,7 +130,9 @@ angular.module("medApp").controller('ComplexControllerOperation', [
 
         }
     }]);
+/******************  Gil *****************************************************************************************/
 
+/******************  SHIR *****************************************************************************************/
 angular.module("medApp").directive("presentTable", function () {
     return {
         restrict: 'E',
@@ -163,7 +175,7 @@ angular.module("medApp").controller("presentTableCtrl", function ($scope, medApp
                 length) + minutesDiff);
     }
 });
-
+/******************  SHIR *****************************************************************************************/
 
 
 
